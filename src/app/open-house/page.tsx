c3c4be31ba3909802a09f5810ac60df7868b82d0
Emmanuel Yankson <110882049+alphaDinar@users.noreply.github.com>
2026-06-18 19:51:21 +0000

    open-house-page

--- src/app/open-house/page.tsx
+++ src/app/open-house/page.tsx
@@ -1,45 +1,15 @@
-"use client";
-import FooterBox from "@/components/footerBox/FooterBox";
-import BookBox from "@/components/home/BookBox";
-import PropertyBox from "@/components/home/PropertyBox";
-import { Button } from "@heroui/button";
-import Image from "next/image";
-import Link from "next/link";
-
-const AboutUs = () => {
+const OpenHouse = () => {
     return (
-        <section className="grid gap-12 sm-screen:gap-6">
-            <section className="mb-5"></section>
-            <section className="box grid grid-cols-2 gap-20 sm-screen:flex sm-screen:flex-col-reverse sm-screen:gap-8">
-                <section className="flex flex-col items-start gap-4 sm-screen:gap-1">
-                    <span className="heading text-primary text-5xl sm-screen:text-2xl">2026 Open House Fair</span>
-
-                    <article className="grid gap-4">
-                        <span>
-                            {"Bijou Homes situated at Appolonia City in the lush suburb of Oyibi, offers 1 and 2 bedroom (expandable to 3) multi-functional, modern homes for young professionals and growing families."}
-                        </span>
-                        <span>
-                            {" At a location which offers superior urban infrastructure, housing and places of work, you have the opportunity to enjoy the comforts of a gated community, 24/7 security, communal  amenities and more at very affordable prices."}
-                        </span>
-                    </article>
-
-
-                    <Button as={Link} href="/about-us" radius="none" variant="bordered" className="flex relative group border w-max text-primary border-primary sm-screen:mt-4 sm-screen:mx-0 sm-screen:px-6 sm-screen:py-3">
-                        <span className="flex z-20 uppercase text-[0.7rem] font-semibold group-hover:text-white">more about us</span>
-                        <div className="bg-transparent w-1 h-full flex absolute left-0 group-hover:bg-primary group-hover:w-full transition-all"></div>
-                    </Button>
-                </section>
-                <section className="h-80 relative md-screen:h-72">
-                    <Image alt="" fill objectFit="cover" src={"https://res.cloudinary.com/dvnemzw0z/image/upload/v1758566871/bijou/BH-FNB-67_fxujbq.jpg"} />
-                </section>
-            </section>
-            <PropertyBox />
-            <section>
-                <BookBox />
-                <FooterBox />
-            </section>
+        <section className="w-full h-screen grid">
+            <iframe
+                title="mapBox"
+                src="https://inkyphotos.pixieset.com/2026bijouhomesopenday/"
+                className="w-full h-screen border-0"
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+            />
         </section>
     );
-}
+};
 
-export default AboutUs;
+export default OpenHouse;
